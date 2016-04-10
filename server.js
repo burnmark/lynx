3c32ccda568c913846609d071c437298a1d29e68
emarkovic <em42@uw.edu>
2016-04-10 10:34:02 -0700

    mariasql package

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-// What other node libraries should be used?
+// if "EADDRINUSE", do: $ sudo kill $(sudo lsof -t -i:80)
 var express = require('express'),
 	app = express(),
 	bcrypt = require('bcrypt'),
-	bodyParser = require('bodyParser'),
+	bodyParser = require('body-parser'),
 	session = require('express-session'),
 	RedisStore = require('connect-redis')(session),
 	morgan = require('morgan'),
-	mysql = require('mysql'),
-	connection = mysql.createConnetion({
-
+	Maria = require('mariasql'),
+	connection = new Maria({
+		host: '127.0.0.1',
+		user: 'host',
+		password: '',
+		db: 'lynx'
 	}),
 	passport = require('passport'),
 	passportLocal = require('passport-local');
@@ -23,4 +26,5 @@ console.log(process.env.SIGSECRET);
 
 var server = app.listen(80, function () {
 	console.log('listening at http://localhost:80');
-})
+});
+
