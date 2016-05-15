6f34d9d0d599c78b53e510cf68d4d55df5219773
emarkovic <em42@uw.edu>
2016-05-15 15:01:20 -0700

    messageStore basic methods

--- app/stores/messageStore.jsx
+++ app/stores/messageStore.jsx
@@ -9,16 +9,42 @@ var _store = {
 };
 
 //0: all sent, 1: all received, 2: all sent and received
-var rawData = Promise.all([]);
+var rawData = Promise.all([
+		$.getJSON('/api/message/sent'),
+		$.getJSON('/api/message/received'), 		
+		$.getJSON('/api/message/')
+	])
+	.then(values => _store.messages = values[0])
+	.then(() => console.log(_store));
 
 var addMessage = function (message) {
 	_store.messages.push(item);
 	// have to make a call to the api to add this message to db
+	// have to refetch data?
 }
 
 var removeMessage = function (index) {
 	_store.messages.splice(index, 1);
-	// have to make a call to the api to delete this message from db
+	// have to make a call to the api to delete this message from db\
+	// have to refetch data?
+}
+
+var filterSent = function () {
+	return awData
+		.then(values => _store.messages = values[0])
+		.then(() => console.log(_store));
+}
+
+var filterReceived = function () {
+	return rawData
+		.then(values => _store.messages = values[1])
+		.then(() => console.log(_store));
+}
+
+var filterAll = function () {
+	return rawData
+		.then(values => _store.messages = values[2])
+		.then(() => console.log(_store))
 }
 
 export default class messageStore {
@@ -50,6 +76,21 @@ AppDispatcher.register(payload => {
 		case appConstants.REMOVE_MESSAGE:
 			removeItem(action.data);
 			messageStore.emit(CHANGE_EVENT);
+			break;
+
+		case appConstants.FILTER_SENT:
+			filterSent()
+				.then(() => messageStore.emit(CHANGE_EVENT));
+			break;
+
+		case appConstants.FILTER_RECEIVED:
+			filterReceived()
+				.then(() => messageStore.emit(CHANGE_EVENT));
+			break;
+
+		case appConstants.FILTER_ALL:
+			filterAll()
+				.then(() => messageStore.emit(CHANGE_EVENT));
 			break;
 
 		default:
