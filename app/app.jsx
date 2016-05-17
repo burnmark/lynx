71d44f2046925063bba576e2560c81af6dca91ac
emarkovic <em42@uw.edu>
2016-05-16 23:09:17 -0700

    title bar checked & changed

--- app/app.jsx
+++ app/app.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import Page from './components/Page.jsx';
-import Avatar from './components/Avatar.jsx';
+import TitleBar from './components/TitleBar.jsx';
 
 // render(<Page sidebarData={sidebarData} messages={messageData} />, document.getElementById('content1'));
-render(<Avatar imgUrl="img/person1.jpg" />, document.getElementById('content1'))
+render(<TitleBar title="img/person1.jpg" />, document.getElementById('content1'))
